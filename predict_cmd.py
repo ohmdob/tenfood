609e52a4aeb2f37f5c44c18218a3dd33fcbb84fd
Frozen <ohm10513@gmail.com>
2021-03-02 19:23:21 +0700

    up

--- predict_cmd.py
+++ predict_cmd.py
@@ -1,7 +1,7 @@
 import predict_image as pi
 
 def predictcmd():
-    return pi.predicting('https://www.angelfire.com/d20/26ddw260/fruit.jpg')
+    return pi.predicting('https://files.sweetlandhub.com/cve7/13647362306584.jpg')
 
 print(predictcmd())
 
